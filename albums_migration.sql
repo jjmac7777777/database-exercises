3ae1dea2c0cf6dc8abc3731bdc6abed5c25bb950
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-08-23 11:17:27 -0500

    database-exercises 8-23-21

---
+++ albums_migration.sql
@@ -0,0 +1,18 @@
+USE codeup_test_db;
+
+DROP IF EXISTS TABLE albums;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50) DEFAULT 'NONE',
+                        name  VARCHAR(100) NOT NULL,
+                        release_date TEXT NOT NULL,
+                        sales int UNSIGNED NOT NULL ,
+                        genre VARCHAR (100),
+                        PRIMARY KEY (id)
+);
+show tables;
+
+DESCRIBE albums;
+
+
