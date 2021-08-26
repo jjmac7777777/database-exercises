c0b2ccfcbe86eac6d6b529e0d027eee46a9ee511
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-08-26 11:15:00 -0500

    Completed GROUP BY Exercises 8-26-21

---
+++ group_by_exercises.sql
@@ -0,0 +1,30 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT title
+FROM employees.titles
+GROUP BY title;
+
+SELECT last_name FROM employees WHERE last_name LIKE 'e%e';
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name;
+
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(last_name), last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT COUNT(gender), gender
+FROM employees
+WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+GROUP BY gender;
+
+
+
+
+
