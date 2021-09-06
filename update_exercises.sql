f11ec1fc6cc78b7fb7e0a0242cb03e8f0df6cf73
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-09-06 15:10:56 -0500

    update_exercises.sql completion

--- update_exercises.sql
+++ update_exercises.sql
@@ -2,3 +2,20 @@ USE codeup_test_db;
 
 SELECT * FROM albums;
 
+SELECT * FROM albums WHERE release_date < 1980;
+
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+UPDATE albums
+SET sales = sales * 10;
+
+UPDATE albums
+SET release_date = 2021
+WHERE release_date < 1980;
+
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson'
+
+
+
